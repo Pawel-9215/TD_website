c1b5621b3a1862543193dabbbcf405db2eeb71c5
Pawel-9215 <hordyniak@gmail.com>
2020-05-26 12:49:50 +0200

    github connection

--- AppTwo/views.py
+++ AppTwo/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from AppTwo.models import User
 from AppTwo.forms import NewUserForm
+import json
+import requests
 
 def help(request):
     my_dick = {'help_script': "This is script for help page"}
@@ -9,6 +11,11 @@ def help(request):
 
 def index(request):
     #return HttpResponse("My Second App")
+    commit_data = requests.get("https://api.github.com/repos/Pawel-9215/TerminalDungeon/commits/master").content
+    commit_data_loaded=json.loads(commit_data)
+    last_commit_date = commit_data_loaded["commit"]["author"]["date"][0:10]
+    last_commit_msg = commit_data_loaded["commit"]["message"]
+
     form = NewUserForm()
     button_s = True
     if request.method == 'POST':
@@ -20,7 +27,7 @@ def index(request):
         else:
             print("ERR in form")
 
-    index_dick = {'form':form, 'button':button_s} 
+    index_dick = {'form':form, 'button':button_s, 'commit_date':last_commit_date, 'commit_msg':last_commit_msg} 
 
     return render(request, "AppTwo/index.html", index_dick)
 
